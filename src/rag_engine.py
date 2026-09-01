80abc5ef6a77fe48049684e286b1772adfc6c2f9
Bagiel1 <gabrielmaiab12@gmail.com>
2026-09-01 11:28:34 -0300

    rag engine primitivo

--- src/rag_engine.py
+++ src/rag_engine.py
@@ -35,14 +35,18 @@ def busca_contexto(pergunta: str, n_resultados: int= 2):
 
     resultados= collection.query(
         query_texts=[pergunta],
-        n_results= 2
+        n_results= n_resultados
     )
+    docs_validos= []
+    nomes_validos= []
 
-    distancias= resultados['distances']
-    textos_encontrados= resultados['documents'][0]
-    nomes_arquivos= [meta['nome_nota'] for meta in resultados['metadatas'][0]]
-
-    return textos_encontrados, nomes_arquivos
+    for doc, distancia, meta in zip(resultados['documents'][0], resultados['distances'][0],
+                                    resultados['metadatas'][0]):
+        if distancia < 1:
+            docs_validos.append(doc)
+            nomes_validos.append(meta['nome_nota'])
+    
+    return docs_validos, nomes_validos
 
 def adicionar_ou_atualizar_nota(full_path: str):
     chroma_client= chromadb.PersistentClient(path="./bunker_db")
